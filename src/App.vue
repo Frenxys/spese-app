60a935f8f49ff00bd53176e1a9a52faae3a6f051
Enea <eneazani2005@gmail.com>
2024-12-02 14:43:12 +0100

    logica implementata

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,79 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Gestione Spese</h1>
+
+    <!-- Form per aggiungere una spesa -->
+    <form @submit.prevent="aggiungiSpesa">
+      <div>
+        <label for="importo">Importo:</label>
+        <input v-model="nuovaSpesa.importo" type="number" id="importo" required />
+      </div>
+      
+      <div>
+        <label for="motivo">Motivo:</label>
+        <input v-model="nuovaSpesa.motivo" type="text" id="motivo" required />
+      </div>
+      
+      <button type="submit">Aggiungi Spesa</button>
+    </form>
+
+    <!-- Tabella delle spese -->
+    <h2>Spese Giornaliere</h2>
+    <table>
+      <thead>
+        <tr>
+          <th>Importo</th>
+          <th>Motivo</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(spesa, index) in spese" :key="index">
+          <td>{{ spesa.importo }} €</td>
+          <td>{{ spesa.motivo }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data() {
+    return {
+      nuovaSpesa: {
+        importo: "",
+        motivo: ""
+      },
+      spese: []
+    };
+  },
+  methods: {
+    aggiungiSpesa() {
+      // Aggiungi la nuova spesa alla lista
+      this.spese.push({
+        importo: parseFloat(this.nuovaSpesa.importo),
+        motivo: this.nuovaSpesa.motivo
+      });
+
+      // Pulisci i campi del form
+      this.nuovaSpesa.importo = "";
+      this.nuovaSpesa.motivo = "";
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+table, th, td {
+  border: 1px solid black;
+}
+th, td {
+  padding: 10px;
+  text-align: left;
 }
 </style>
